6b6b6d91b68d4787bc2c76a303e15c24d2273a77
yueh <starandsea.ren@gmail.com>
2016-12-26 20:34:02 +0800

    架构搭建ing
    架构搭建ing

---
+++ perch/src/main/java/ren/yueh/perch/BaseAndroidView.java
@@ -0,0 +1,23 @@
+/**
+ * File name: BaseAndroidView
+ * Author: Yueh
+ * Date: 2016/12/26 18:03
+ * E-mail: yueh.r@foxmail.com
+ * GitHub: https://github.com/StarAndSea-Ren
+ */
+package ren.yueh.perch;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Description：Android view模板超类
+ * <p>
+ * Edit date:
+ * Edit content:
+ */
+public abstract class BaseAndroidView extends View {
+    public BaseAndroidView(Context context) {
+        super(context);
+    }
+}
---
+++ perch/src/main/java/ren/yueh/perch/Perch.java
@@ -0,0 +1,18 @@
+/**
+ * File name: Perch
+ * Author: Yueh
+ * Date: 2016/12/26 17:09
+ * E-mail: yueh.r@foxmail.com
+ * GitHub: https://github.com/StarAndSea-Ren
+ */
+package ren.yueh.perch;
+
+/**
+ * Description：Perch 入口
+ * <p>
+ * Edit date:2016-12-26 17:10:55
+ * Edit content:
+ */
+public class Perch {
+
+}
